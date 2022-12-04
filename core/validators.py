c44d112a7cd5f4906e8bd22a66c1383c69f1bcdd
Janilson Varela <janilsonjvs@gmail.com>
2022-12-04 20:30:52 -0300

    feat: validate number only

--- core/validators.py
+++ core/validators.py
@@ -1,3 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import  RegexValidator
+
+def number_only(value):
+    RegexValidator(r'^[0-9]*$', 'Somente números.').__call__(value)
 
