fe8d5a3d26e1fac888b5e84207fe413655d43e71
Janilson Varela <janilsonjvs@gmail.com>
2022-12-04 14:29:11 -0300

    feat: organizando os fields no form da cidade

--- core/admin.py
+++ core/admin.py
@@ -11,6 +11,7 @@ class EstadoAdmin(admin.ModelAdmin):
 @admin.register(Cidade)
 class CidadeAdmin(admin.ModelAdmin):
     list_display = ('nome', 'estado')
+    fields = (('nome', 'estado'), 'cod_ibge')
 
 
 @admin.register(Cliente)
