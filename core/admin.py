10473b7793d91763748c2644a6a0c06efe6e7d9e
Janilson Varela <janilsonjvs@gmail.com>
2022-12-04 11:27:25 -0300

    feat: registrando no admin o model cidade

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Estado
+from .models import Estado, Cidade
 
 
 @admin.register(Estado)
 class EstadoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'uf')
 
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'estado')
