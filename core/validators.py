68d0da5fd380b7f020d8607ab0de716e7f02f6fb
Janilson Varela <janilsonjvs@gmail.com>
2022-12-04 20:42:21 -0300

    feat: validator letter only

--- core/validators.py
+++ core/validators.py
@@ -5,3 +5,5 @@ from django.core.validators import  RegexValidator
 def number_only(value):
     RegexValidator(r'^[0-9]*$', 'Somente números.').__call__(value)
 
+def letter_only(value):
+    RegexValidator(r'^[a-zA-Z\s]*$', 'Somente letras.').__call__(value)
