386c03121c4d1193aed75e8442a7242ec0e7abec
Janilson Varela <janilsonjvs@gmail.com>
2022-12-05 15:40:34 -0300

    feat: config em settings as constantes dos arquivos static

--- financa_django/settings.py
+++ financa_django/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,6 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
