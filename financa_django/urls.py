43d740dbb4fe3da1dc00c962a354005a09cb1abd
Janilson Varela <janilsonjvs@gmail.com>
2022-12-06 20:30:50 -0300

    feat: url cidade for estado

--- financa_django/urls.py
+++ financa_django/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from core.views import cidades
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/cidades/<int:estado>', cidades)
+    
 ]
