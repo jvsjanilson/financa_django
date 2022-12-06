996078c7bcb92187695c7488510b17667ab62860
Janilson Varela <janilsonjvs@gmail.com>
2022-12-06 20:30:22 -0300

    feat: js load cidade of estado using jquery and ajax

---
+++ core/static/js/cliente.js
@@ -0,0 +1,19 @@
+django.jQuery().ready(function(){
+    django.jQuery("#id_estado").on('change', function(e) {
+        django.jQuery("#id_cidade option").remove();
+        django.jQuery("#id_cidade").append("<option value>---------</option>");
+        django.jQuery.ajax({
+            method: 'get',
+            url: '/api/cidades/'+e.target.value,
+            success: (res) => {
+                JSON.parse(res).forEach(cidade => {
+                    django.jQuery("#id_cidade").append(`
+                        <option ${cidade.fields.capital ? 'selected' : ''} 
+                            value=${cidade.pk}>${cidade.fields.nome}
+                        </option>
+                    `);
+                });
+            }
+        });
+    });
+});
