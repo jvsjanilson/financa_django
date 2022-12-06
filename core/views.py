a169a360dabf82cc63dc16f28239531489e3fd24
Janilson Varela <janilsonjvs@gmail.com>
2022-12-06 20:29:40 -0300

    feat: add field id of cidade

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
+
+from .models import Cidade
+import json
 
 # Create your views here.
+
+def cidades(request, estado):
+    data = serialize("json", Cidade.objects.filter(estado=estado), fields=('nome', 'capital'))
+    return HttpResponse(data)
+
+    
