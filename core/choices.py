70464faffb2227e7b7e1cc61f401733303284bb9
Janilson Varela <janilsonjvs@gmail.com>
2022-12-07 19:54:55 -0300

    feat: novo type situacao

--- core/choices.py
+++ core/choices.py
@@ -10,3 +10,7 @@ class TypeIntervalo(models.TextChoices):
     DIARIO = 'D', _('Diário')
     MENSAL = 'M', _('Mensal')
     ANUAL = 'A', _('Anual')
+
+class TypeSituacao(models.TextChoices):
+    ABERTO = 'A', _('Aberto')
+    PAGO = 'P', _('Pago')
