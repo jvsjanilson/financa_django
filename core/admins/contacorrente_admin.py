ea7dd16be6770781ae668f226a398188fda57628
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:37:51 -0300

    feat: separando class contacorrente admin

---
+++ core/admins/contacorrente_admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import ContaCorrente
+
+@admin.register(ContaCorrente)
+class ContaCorrenteAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'banco', 'numero_agencia', 'numero_conta', 'data_abertura', 'data_fechamento', 'saldo', 'ativo')
+    fields = (('descricao','banco'), ('numero_conta','numero_agencia'), ('data_abertura','data_fechamento'), 'saldo', 'ativo')
