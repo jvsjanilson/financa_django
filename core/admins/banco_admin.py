e37e5869a685b9654544222e1313f2c755b73226
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:36:20 -0300

    feat: separando a class banco admin

---
+++ core/admins/banco_admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core.models import Banco
+
+@admin.register(Banco)
+class BancoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao')
+    fields = ('codigo', 'descricao')
+
+    class Media:
+        js = ("admin/js/jquery.init.js", "admin/js/vendor/jquery/jquery.js", "js/banco.js",)
