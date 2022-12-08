70676f090f93573e51a00a07ab3a9f473ac7bc0a
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:34:36 -0300

    feat: separando a class condicaopagamento admin

---
+++ core/admins/condicaopagamento_admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from core.models import CondicaoPagamento
+
+@admin.register(CondicaoPagamento)
+class CondicaoPagamentoAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'formapagamento', 'tipo_intervalo', 'dia_fixo', 'ativo')
+    fields = (('descricao', 'formapagamento'),  ('qtd_max_parcela', 'tipo_intervalo','intervalo'), 'dia_fixo', 'ativo')
+
