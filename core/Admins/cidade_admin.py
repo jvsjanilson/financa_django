80fdac764ffab95ea4f85fcef697a37a31e5f6ab
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 17:09:36 -0300

    feat: search cidade

--- core/Admins/cidade_admin.py
+++ core/Admins/cidade_admin.py
@@ -5,4 +5,5 @@ from core.models import Cidade
 class CidadeAdmin(admin.ModelAdmin):
     list_display = ('nome', 'estado')
     fields = (('nome', 'estado'), 'cod_ibge', 'capital')
+    search_fields = ('nome',)
     
