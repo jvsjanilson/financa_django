e71ba4bb419fa0f103720e57ee254504cbd3577d
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:28:59 -0300

    feat: class separada para estado admin

---
+++ core/admins/estado_admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from core.models import Estado
+
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'uf')
+    fields = (('uf', 'nome'), 'cod_ibge')
