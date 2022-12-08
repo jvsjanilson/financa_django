d1ae385b8ac47450a9103f3d0d16a90d0bac9cf1
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 17:03:22 -0300

    feat: search banco

--- core/Admins/banco_admin.py
+++ core/Admins/banco_admin.py
@@ -5,6 +5,7 @@ from core.models import Banco
 class BancoAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'descricao')
     fields = ('codigo', 'descricao')
+    search_fields = ('codigo', 'descricao')
 
     class Media:
         js = ("admin/js/jquery.init.js", "admin/js/vendor/jquery/jquery.js", "js/banco.js",)
