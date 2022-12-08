f355709c0aba2e7201b5a78a86dde1c90bd887b9
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:21:20 -0300

    feat: actions para ativar e desativar cliente

--- core/admin.py
+++ core/admin.py
@@ -22,9 +22,34 @@ class ClienteAdmin(admin.ModelAdmin):
     ('logradouro', 'numero', 'cep'), ('complemento', 'bairro'), ('estado', 'cidade'),
     ('fone', 'celular', 'email'), 'ativo')
 
+    actions = ('desativar', 'ativar')
+
     class Media:
         js = ("admin/js/jquery.init.js", "admin/js/vendor/jquery/jquery.js", "js/cliente.js",)
 
+    def desativar(self, request, queryset):
+        count = queryset.update(ativo = 0)
+
+        if count == 1:
+            message = '{} cliente foi desativado.'
+        else:
+            message = '{} clientes foram desativados.'
+        
+        self.message_user(request, message.format(count))
+
+    desativar.short_description = 'Desativar cliente(s)'
+
+    def ativar(self, request, queryset):
+        count = queryset.update(ativo = 1)
+
+        if count == 1:
+            message = '{} cliente foi ativado.'
+        else:
+            message = '{} clientes foram ativados.'
+        
+        self.message_user(request, message.format(count))
+
+    ativar.short_description = 'Ativar cliente(s)'
 
 @admin.register(FormaPagamento)
 class FormaPagamentoAdmin(admin.ModelAdmin):
