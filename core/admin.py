7087dc6e32aac37e6092a6b8f363e69685fe549c
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:57:47 -0300

    feat: renomeando pasta admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-import core.admins.estado_admin
-import core.admins.cliente_admin
-import core.admins.cidade_admin
-import core.admins.formapagamento_admin
-import core.admins.condicaopagamento_admin
-import core.admins.banco_admin
-import core.admins.contacorrente_admin
-import core.admins.contareceber_admin
+import core.Admins.estado_admin
+import core.Admins.cliente_admin
+import core.Admins.cidade_admin
+import core.Admins.formapagamento_admin
+import core.Admins.condicaopagamento_admin
+import core.Admins.banco_admin
+import core.Admins.contacorrente_admin
+import core.Admins.contareceber_admin
