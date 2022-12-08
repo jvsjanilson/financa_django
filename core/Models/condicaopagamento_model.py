c70d4d9eab025b884bad262815c1971e485ffc9e
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 18:25:53 -0300

    feat: verbose_name nos fields

--- core/Models/condicaopagamento_model.py
+++ core/Models/condicaopagamento_model.py
@@ -5,12 +5,12 @@ from core.choices import TypeIntervalo
 
 
 class CondicaoPagamento(models.Model):
-    descricao = models.CharField(max_length=60)
-    formapagamento = models.ForeignKey(FormaPagamento, on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=60, verbose_name='Descrição')
+    formapagamento = models.ForeignKey(FormaPagamento, on_delete=models.CASCADE, verbose_name='Formas de pagamento')
     qtd_max_parcela = models.SmallIntegerField(default=1, validators=[MinValueValidator(1, 'Valor minimo é 1.')])
     tipo_intervalo = models.CharField(max_length=2, choices=TypeIntervalo.choices, default=TypeIntervalo.DIARIO)
     intervalo = models.SmallIntegerField(default=0, validators=[MinValueValidator(0, 'Valor mínimo é 0.')])
-    dia_fixo = models.BooleanField(default=False)
+    dia_fixo = models.BooleanField(default=False, verbose_name='Dia Fixo?')
     ativo = models.BooleanField(default=True)
 
     def __str__(self) -> str:
