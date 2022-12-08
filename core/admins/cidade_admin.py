f96fd74f05bf130cb5aa92609bc473bb1b53c326
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:31:21 -0300

    feat: separando class cidade admin

---
+++ core/admins/cidade_admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import Cidade
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'estado')
+    fields = (('nome', 'estado'), 'cod_ibge', 'capital')
