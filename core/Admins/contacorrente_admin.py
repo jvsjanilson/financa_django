3db3920ce0c11c5d2424354b81187765d6109d17
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 17:09:57 -0300

    feat: search conta corrente

--- core/Admins/contacorrente_admin.py
+++ core/Admins/contacorrente_admin.py
@@ -5,3 +5,5 @@ from core.models import ContaCorrente
 class ContaCorrenteAdmin(admin.ModelAdmin):
     list_display = ('descricao', 'banco', 'numero_agencia', 'numero_conta', 'data_abertura', 'data_fechamento', 'saldo', 'ativo')
     fields = (('descricao','banco'), ('numero_conta','numero_agencia'), ('data_abertura','data_fechamento'), 'saldo', 'ativo')
+    search_fields = ('descricao', 'numero_agencia', 'numero_conta')
+    
