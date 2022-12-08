eb8d98da15a771ceb0a77c3b6e40f37d348278c6
Janilson Varela <janilsonjvs@gmail.com>
2022-12-08 08:32:53 -0300

    feat: separando class formapagamento admin

---
+++ core/admins/formapagamento_admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import FormaPagamento
+
+@admin.register(FormaPagamento)
+class FormaPagamentoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'tipo')
+    fields = (('codigo', 'descricao'), 'tipo')
