52a8141305654c58f545546ac19a331b48f1c974
Janilson Varela <janilsonjvs@gmail.com>
2022-12-12 17:53:11 -0300

    feat: cpfcnpj unico em cliente

--- core/Models/cliente_model.py
+++ core/Models/cliente_model.py
@@ -1,11 +1,13 @@
 from django.db import models
 from core.Models.estado_model import Estado
 from core.Models.cidade_model import Cidade
+from core.validators import valida_cpfcnpj
+
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=60)
     nome_fantasia = models.CharField(max_length=60, blank=True, null=True)
-    cpfcnpj = models.CharField(max_length=14, blank=True, null=True, verbose_name='CPF/CNPJ')
+    cpfcnpj = models.CharField(max_length=14, blank=True, null=True, validators=[valida_cpfcnpj], verbose_name='CPF/CNPJ', unique=True)
     insc_estadual = models.CharField(max_length=14, blank=True, null=True)
     insc_municipal = models.CharField(max_length=20, blank=True, null=True)
     logradouro = models.CharField(max_length=60, blank=True, null=True)
