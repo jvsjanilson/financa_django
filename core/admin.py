cd7e7a62fbf8efa23482f6446c0034fd1a9a8bce
Janilson Varela <janilsonjvs@gmail.com>
2022-12-12 17:13:50 -0300

    feat: registrando o admin fornecedor em admin.py

--- core/admin.py
+++ core/admin.py
@@ -7,3 +7,4 @@ import core.Admins.condicaopagamento_admin
 import core.Admins.banco_admin
 import core.Admins.contacorrente_admin
 import core.Admins.contareceber_admin
+import core.Admins.fornecedor_admin
